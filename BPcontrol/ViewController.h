f3707d271284d50ee8929ec45f1841975bb36901
ancarrera <ancarrera31@gmail.com>
2015-04-14 00:10:37 +0200

    init commit and add ruby cocoa pods

---
+++ BPcontrol/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BPcontrol
+//
+//  Created by Adrian on 13/4/15.
+//  Copyright (c) 2015 Adrian Carrera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BPcontrol/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BPcontrol
+//
+//  Created by Adrian on 13/4/15.
+//  Copyright (c) 2015 Adrian Carrera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
