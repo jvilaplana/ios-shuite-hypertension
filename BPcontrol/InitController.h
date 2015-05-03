02d2f627ebd907c6014f1e8ff835e7fe185350bf
ancarrera <ancarrera31@gmail.com>
2015-05-03 17:48:46 +0200

    tab bar OK

---
+++ BPcontrol/InitController.h
@@ -0,0 +1,13 @@
+//
+//  InitController.h
+//  BPcontrol
+//
+//  Created by Adrian Carrera on 3/5/15.
+//  Copyright (c) 2015 Adrian Carrera. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface InitController : ViewController
+
+@end
---
+++ BPcontrol/InitController.m
@@ -0,0 +1,37 @@
+//
+//  InitController.m
+//  BPcontrol
+//
+//  Created by Adrian Carrera on 3/5/15.
+//  Copyright (c) 2015 Adrian Carrera. All rights reserved.
+//
+
+#import "InitController.h"
+
+@interface InitController ()
+
+@end
+
+@implementation InitController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
